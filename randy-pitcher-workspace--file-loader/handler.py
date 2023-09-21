58f1c8fd828b5499e1f809dab620f5cb6c23b317
🌞Randy Pitcher <randypitcherii@gmail.com>
2023-09-21 16:02:43 -0400

    implemented a json scraper against openweather

--- randy-pitcher-workspace--file-loader/handler.py
+++ randy-pitcher-workspace--file-loader/handler.py
@@ -1,11 +1,45 @@
-import datetime
-import logging
+import boto3
+import json
+import requests
+from datetime import datetime
+import uuid
+import os
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+api_key = os.environ["OPENWEATHER_API_KEY"]
+s3_bucket_name = "randy-pitcher-workspace--aws"
+s3_folder_location = 'tabular/staged/rpw__aws/default/weather_raw'
 
 
-def run(event, context):
-    current_time = datetime.datetime.now().time()
-    name = context.function_name
-    logger.info("Your cron function " + name + " ran at " + str(current_time))
+def get_weather(city, api_key):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "q": city,
+        "appid": api_key,
+        "units": "imperial",  # use 'metric' for Celsius
+    }
+    try:
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred for city {city}: {http_err}")
+        return None
+    except Exception as err:
+        print(f"Other error occurred for city {city}: {err}")
+        return None
+    else:
+        return response.json()
+
+
+def lambda_handler(event, context):
+    s3 = boto3.client("s3")
+
+    cities = ["Indianapolis", "New York", "London", "Paris", "Tokyo", "Sydney"]
+
+    for city in cities:
+        weather_data = get_weather(city, api_key)
+
+        if weather_data:
+            file_name = f"{city}--{datetime.utcnow().strftime('%Y%m%d%H%M%S%f')}--{uuid.uuid4()}.json"
+            s3.put_object(
+                Bucket=s3_bucket_name, Key=f"{s3_folder_location}/{file_name}", Body=json.dumps(weather_data)
+            )
