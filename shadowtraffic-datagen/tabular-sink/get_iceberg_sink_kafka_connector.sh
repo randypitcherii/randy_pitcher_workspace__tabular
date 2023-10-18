975451988850aa816fb9660ef1e93fa4bacda9f1
🌞Randy Pitcher <randypitcherii@gmail.com>
2023-10-18 10:53:54 -0400

    migrated shadow work to this workspace

---
+++ shadowtraffic-datagen/tabular-sink/get_iceberg_sink_kafka_connector.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Download the zip file
+wget https://github.com/tabular-io/iceberg-kafka-connect/releases/download/v0.5.7/iceberg-kafka-connect-runtime-0.5.7.zip
+
+# Extract the zip file
+unzip iceberg-kafka-connect-runtime-0.5.7.zip
