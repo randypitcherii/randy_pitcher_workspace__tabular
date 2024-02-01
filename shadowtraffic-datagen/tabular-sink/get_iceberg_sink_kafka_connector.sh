d663bd6e364ee42c8ea88ac161b3ce5f0edba069
🌞Randy Pitcher <randypitcherii@gmail.com>
2024-02-01 16:06:43 -0500

    transitioned to the more reliable distributed kafka connect for the tabular sink

--- shadowtraffic-datagen/tabular-sink/get_iceberg_sink_kafka_connector.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VERSION="0.5.8"
-
-# Download the zip file
-wget "https://github.com/tabular-io/iceberg-kafka-connect/releases/download/v${VERSION}/iceberg-kafka-connect-runtime-${VERSION}.zip"
-
-# Extract the zip file
-unzip "iceberg-kafka-connect-runtime-${VERSION}.zip"
